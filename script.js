4cb2d485fb1077efe1bfd2b79352cc90f43c810d
Firas Hamdouni <firas.hamdouni@spectrumgroupe.fr>
2021-02-22 10:31:42 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+const li = document.querySelectorAll('li.dropdown a');
+const btn = document.querySelector('.nav-btn');
+const nav = document.querySelector('ul.nav');
+
+btn.addEventListener('click', e=>{
+    nav.classList.toggle('toggle');
+})
+
+
+li.forEach((each)=>{
+  console.log(each)
+    if (each.nextElementSibling !== null) {
+      console.log(each.nextElementSibling)
+        each.addEventListener('click', e=>{
+            if (window.innerWidth < 768) {
+              e.target.parentElement.classList.toggle("active");  
+            }
+        })
+    }
+})
